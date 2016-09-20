90256b265bfe4fc7a96ebac629f70bd759e1e2b2
1316143 <franchescoli.ramirez@tcs.com>
2016-09-20 18:48:27 -0500

    Adaptando JNDI

--- rc-mobile-project/rc-mobile-project-web/src/main/java/pe/com/rc/mobile/web/config/DataSourceConfig.java
+++ rc-mobile-project/rc-mobile-project-web/src/main/java/pe/com/rc/mobile/web/config/DataSourceConfig.java
@@ -11,13 +11,13 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 @ComponentScan({ "pe.com.rc.mobile.*" })
 public class DataSourceConfig {
 
-	@Bean
-	public DataSource getDataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/contactdb");
-		dataSource.setUsername("root");
-		dataSource.setPassword("root");
-		return dataSource;
-	}
+//	@Bean
+//	public DataSource getDataSource() {
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//		dataSource.setUrl("jdbc:mysql://localhost:3306/contactdb");
+//		dataSource.setUsername("root");
+//		dataSource.setPassword("root");
+//		return dataSource;
+//	}
 }
