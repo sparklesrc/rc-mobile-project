acee7d1403574d4051dca55af8b48a84ee04cf92
Franchesco <framirez@creditsesame.com>
2018-01-22 18:25:03 -0500

    Add User

---
+++ rc-mobile-project/rc-mobile-project-dao/src/main/java/pe/com/rc/mobile/dao/UserDAO.java
@@ -0,0 +1,8 @@
+package pe.com.rc.mobile.dao;
+
+import pe.com.rc.mobile.dao.helper.GenericDAO;
+import pe.com.rc.mobile.model.User;
+
+public interface UserDAO extends GenericDAO<User> {
+
+}
---
+++ rc-mobile-project/rc-mobile-project-dao/src/main/java/pe/com/rc/mobile/dao/impl/UserDAOH.java
@@ -0,0 +1,38 @@
+package pe.com.rc.mobile.dao.impl;
+
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+
+import pe.com.rc.mobile.dao.UserDAO;
+import pe.com.rc.mobile.dao.helper.BaseHibernateDAO;
+import pe.com.rc.mobile.model.User;
+
+@Repository
+public class UserDAOH extends BaseHibernateDAO implements UserDAO {
+
+	public User find(User t) {
+		Criteria criteria = this.getSession().createCriteria(User.class);
+		criteria.add(Restrictions.eq("id", t.getId()));
+		return (User) criteria.uniqueResult();
+	}
+
+	public List<User> all() {
+		Criteria criteria = this.getSession().createCriteria(User.class);
+        return criteria.list();
+	}
+
+	public void save(User t) {
+		this.getSession().save(t);
+	}
+
+	public void update(User t) {
+		this.getSession().merge(t);
+	}
+
+	public void delete(User t) {
+		this.getSession().delete(t);
+	}
+
+}
