dbc28c1e3a02dfacf6eb22c9e0437d3cf5fc0e43
Franchesco <framirez@creditsesame.com>
2018-01-26 15:46:00 -0500

    userClanId

---
+++ rc-mobile-project/rc-mobile-project-model/src/main/java/pe/com/rc/mobile/model/UserClanId.java
@@ -0,0 +1,33 @@
+package pe.com.rc.mobile.model;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class UserClanId implements Serializable {
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	private User user;
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Clan clan;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Clan getClan() {
+		return clan;
+	}
+
+	public void setClan(Clan clan) {
+		this.clan = clan;
+	}
+
+}
